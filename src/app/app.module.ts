e3d60cc9f7773622c2e69f76822341f0332e2b0c
Harry Nguyen <phuquyit@gmial.com>
2020-07-04 17:26:04 +0700

    Ứng dụng calculator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.component';
 import { PetComponent } from './pet/pet.component';
 import { LoginBoostrapComponent } from './login-boostrap/login-boostrap.component';
+import { CalculatorComponent } from './calculator/calculator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FontSizeEditorComponent,
     PetComponent,
-    LoginBoostrapComponent
+    LoginBoostrapComponent,
+    CalculatorComponent
   ],
   imports: [
     BrowserModule
